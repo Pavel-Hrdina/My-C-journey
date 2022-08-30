8c544ba1ddf03eb0f05b7cd1acb21f1bf4faf808
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2022-08-30 16:43:39 +0200

    feat(Exercises): Exercise 1 for chapter 5

---
+++ Chapter_5_variables_and_declarations/Code/exercises/exercise_1.c
@@ -0,0 +1,24 @@
+/**
+ * @file exercise_1.c
+ * @author Pavel Hrdina
+ * @brief
+ * @version 0.1
+ * @date 2022-08-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int start_temperature, end_temperature, norman, my_int;
+    float my_float = 23.1256;
+
+    my_int = (int)my_float;
+
+    norman = 1066;
+
+    return printf("Only the number %d\n", my_int);
+}
