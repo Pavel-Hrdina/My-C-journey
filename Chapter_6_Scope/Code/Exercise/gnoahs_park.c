e8bdb4d35b8e6393a11c8bbf29d1e34264d95d26
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2022-08-31 20:54:17 +0200

    feat(Code): Code for chapter 6

---
+++ Chapter_6_Scope/Code/Exercise/gnoahs_park.c
@@ -0,0 +1,26 @@
+/**
+ * @file gnoahs_park.c
+ * @author Pavel Hrdina
+ * @brief
+ * @version 0.1
+ * @date 2022-08-31
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int num_gnus, num_gnats;
+
+int main()
+{
+    float avg_gnu_mass, avg_gnat_mass;
+
+    return 0;
+}
+
+int calculate_park_biomass(avg_gnu_mass, avg_gnat_mass)
+{
+    return 0;
+}
