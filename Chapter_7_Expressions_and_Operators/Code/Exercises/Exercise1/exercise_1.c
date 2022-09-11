13ddbde6fd85e0a01c5ff50d68d16b11c951eba4
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2022-09-11 11:24:38 +0200

    feat(Exercises): Exercises for chapter 7

---
+++ Chapter_7_Expressions_and_Operators/Code/Exercises/Exercise1/exercise_1.c
@@ -0,0 +1,19 @@
+/**
+ * @file exercise_1.c
+ * @author Pavel Hrdina
+ * @brief A short statement that assigns the remainder of 5 divided by 2 to a variable called remainder and prints it out.
+ * @version 0.1
+ * @date 2022-09-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int reminds = (5 / 2);
+
+    return printf("Reminder is %d\n", reminds);
+}
